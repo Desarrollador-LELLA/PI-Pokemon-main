e88dd68ee50ed9ca67cc024788b59feff8a1fbd8
Luis Llancamil <luis.llancamil.a@gmail.com>
2023-01-06 05:03:18 -0300

    Tester Act.

--- client/src/components/com/Detalle.jsx
+++ client/src/components/com/Detalle.jsx
@@ -6,10 +6,12 @@ const Detalle = ({ detalle }) => {
     const { id, nombre, vida, ataque, defenza, velocidad, altura, peso, imagen, tipos } = detalle;
 
     const metodo = (valor, div) => {
+        console.log(div)
         const ancho = (valor / 280) * 100;
         if (div) {
             // console.log(div.style.getPropertyValue("--valor-nivel"));
             div.style.setProperty("--valor-nivel", `${ancho}%`);
+            // div.style.setProperty("@value valor:", `${ancho}%`);
             // div.setAttribute("style", `width: ${ancho.toString()}%; background: rgb(173, 255, 241); height: 100%;`);
         }
     };
@@ -42,6 +44,7 @@ const Detalle = ({ detalle }) => {
                             <div className={s.barranombre}>Vida</div>
                             <div className={s.barrastat}>
                                 <div className={s.nivel} id='vida' >
+                                    {console.log(s.nivel.toString())}
                                     {metodo(vida, document.getElementById('vida'))}
                                     <div>{vida}</div>
                                 </div>
