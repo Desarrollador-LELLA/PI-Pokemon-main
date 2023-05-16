8c89fc32e718b1321d8830252a1844f6ebdd3d43
Luis Esteban Llancamil Aguilera <luis.llancamil.a@gmail.com>
2023-05-16 00:01:45 -0400

    Actualizacion

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,29 @@ import { Route, Routes } from 'react-router-dom';
 import Landing from './components/pages/Landing';
 import Layout from './components/pages/Layout';
 import Home from './components/pages/Home';
-import CreaPokemon from './components/pages/CreaPokemon'
-import DetallePokemon from './components/pages/DetallePokemon'
+import CreaPokemon from './components/pages/CreaPokemon';
+import DetallePokemon from './components/pages/DetallePokemon';
 import Busqueda from './components/pages/Busqueda';
 import PaginaNoFount from './components/pages/PaginaNoFount';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  return (
-    <Routes>
-      <Route path='/' element={<Landing />} />
-      <Route path='*' element={<PaginaNoFount />} />
-      <Route path='/pokemons' element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='busqueda/:nombre' element={<Busqueda />} />
-        <Route path='detalle/:id' element={<DetallePokemon />} />
-        <Route path='crear' element={<CreaPokemon />} />
-        <Route path='*' element={<PaginaNoFount />} />
-      </Route>
-    </Routes>
-  );
+   return (
+      <>
+         <Analytics />
+         <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='*' element={<PaginaNoFount />} />
+            <Route path='/pokemons' element={<Layout />}>
+               <Route index element={<Home />} />
+               <Route path='busqueda/:nombre' element={<Busqueda />} />
+               <Route path='detalle/:id' element={<DetallePokemon />} />
+               <Route path='crear' element={<CreaPokemon />} />
+               <Route path='*' element={<PaginaNoFount />} />
+            </Route>
+         </Routes>
+      </>
+   );
 }
 
 export default App;
